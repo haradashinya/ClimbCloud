007e5cca87c4aed80d1381f45c49c875fba12491
shinyaharada <haradashinya@gmail.com>
2019-08-24 17:17:06 +0900

    Add force

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 340.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        int key = 0;
+        if(Input.GetKeyDown(KeyCode.RightArrow)) key = 1;
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) key =- 1;
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+        if (speedx < this.maxWalkSpeed){
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+
+
+    }
+}
